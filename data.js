32e183d2e9aa2ad1e9ff40caddd3d5f8bd893290
a-vargasmarte <a.vargasmarte@gmail.com>
2019-03-22 13:42:16 -0400

    initial commit- added all prepared dataviz files

---
+++ data.js
@@ -0,0 +1,479 @@
+let data = {
+  name: "Survey Questions",
+  children: [
+    {
+      name: "Are you Hispanic or Latino?",
+      children: [
+        {
+          name: "yes",
+          count: 470
+        },
+        {
+          name: "no",
+          count: 922
+        },
+        {
+          name: "chose not to answer",
+          count: 4
+        }
+      ]
+    },
+    {
+      name: "Which race are you? Select all that apply",
+      children: [
+        {
+          name: "Native Hawaiian",
+          count: 2
+        },
+        {
+          name: "American Indian/Alaskan Native",
+          count: 4
+        },
+        {
+          name: "Asian",
+          count: 9
+        },
+        {
+          name: "I chose not to answer",
+          count: 72
+        },
+        {
+          name: "White",
+          count: 236
+        },
+        {
+          name: "Other",
+          count: 246
+        },
+        {
+          name: "Black/African American",
+          count: 605
+        }
+      ]
+    },
+    {
+      name:
+        "At any point in the past 2 years, has seasonal or migrant farm been your or your family's main source of income?",
+      children: [
+        {
+          name: "I choose not to answer",
+          count: 8
+        },
+        {
+          name: "yes",
+          count: 15
+        },
+        {
+          name: "no",
+          count: 1365
+        }
+      ]
+    },
+    {
+      name: "What language are you most comfortable speaking?",
+      children: [
+        {
+          name: "English",
+          count: 920
+        },
+        {
+          name: "Spanish",
+          count: 375
+        },
+        {
+          name: "Other",
+          count: 17
+        },
+        {
+          name: "Arabic",
+          count: 5
+        },
+        {
+          name: "Italian",
+          count: 1
+        },
+        {
+          name: "French",
+          count: 1
+        },
+        {
+          name: "Indian",
+          count: 1
+        }
+      ]
+    },
+    {
+      name:
+        "How many family members, including yourself do you currently live with?",
+      children: [
+        {
+          name: "1",
+          count: 349
+        },
+        {
+          name: "2",
+          count: 227
+        },
+        {
+          name: "3-4",
+          count: 358
+        },
+        {
+          name: "5-8",
+          count: 225
+        },
+        {
+          name: "More than 8",
+          count: 11
+        },
+        {
+          name: "I choose not to answer",
+          count: 1
+        }
+      ]
+    },
+    {
+      name: "What is your housing situation today",
+      children: [
+        {
+          name: "I have housing",
+          count: 1123
+        },
+        {
+          name: "I do not have housing",
+          count: 249
+        },
+        {
+          name: "I choose not to answer",
+          count: 15
+        }
+      ]
+    },
+    {
+      name: "Are you worried about losing your housing?",
+      children: [
+        {
+          name: "I choose not to answer",
+          count: 20
+        },
+        {
+          name: "yes",
+          count: 224
+        },
+        {
+          name: "no",
+          count: 1136
+        }
+      ]
+    },
+    {
+      name: "What is the highest level of school that you have finished",
+      children: [
+        {
+          name: "I chose not to answer",
+          count: 10
+        },
+        {
+          name: "Less than Highschool",
+          count: 452
+        },
+        {
+          name: "Highschool or GED",
+          count: 430
+        },
+        {
+          name: "More than highschool",
+          count: 496
+        }
+      ]
+    },
+    {
+      name: "What is your main insurance?",
+      children: [
+        {
+          name: "private insurance",
+          count: 69
+        },
+        {
+          name: "chip medicaid",
+          count: 3
+        },
+        {
+          name: "public insurance-chip",
+          count: 19
+        },
+        {
+          name: "medicaid",
+          count: 187
+        },
+        {
+          name: "public insurance-not chip",
+          count: 128
+        },
+        {
+          name: "medicare",
+          count: 214
+        },
+        {
+          name: "none/uninsured",
+          count: 86
+        }
+      ]
+    },
+    {
+      name:
+        "During the past year, what was the total combined income for you and the family members you live with?",
+      children: [
+        {
+          name: "$75,000 - $100,000",
+          count: 9
+        },
+        {
+          name: "$50,000 - $75,000",
+          count: 7
+        },
+        {
+          name: "$40,000 - $50,000",
+          count: 21
+        },
+        {
+          name: "$30,000 - $40,000",
+          count: 46
+        },
+        {
+          name: "$25,000 - $30,000",
+          count: 49
+        },
+        {
+          name: "$20,000 - $25,000",
+          count: 55
+        },
+        {
+          name: "$15,000 - $20,000",
+          count: 86
+        },
+        {
+          name: "$10,000 - $15,000",
+          count: 117
+        },
+        {
+          name: "$5,000 - $10,000",
+          count: 115
+        },
+        {
+          name: "$1,000 - $5,000",
+          count: 33
+        },
+        {
+          name: "under $1,000",
+          count: 62
+        }
+      ]
+    },
+    {
+      name:
+        "In the past year, have you or someone in your household had to go without any of the following when it was really needed? Check all that apply.",
+      children: [
+        {
+          name: "child care",
+          count: 30
+        },
+        {
+          name: "transportation",
+          count: 43
+        },
+        {
+          name: "phone",
+          count: 57
+        },
+        {
+          name: "utilities",
+          count: 116
+        },
+        {
+          name: "clothing",
+          count: 79
+        },
+        {
+          name: "food",
+          count: 242
+        },
+        {
+          name: "medicare or medical care",
+          count: 288
+        }
+      ]
+    },
+    {
+      name:
+        "OPTIONAL: In the past year, have you or someone in your household had to go without any of the following when it was really needed? Check all that apply.",
+      children: [
+        {
+          name: "rent/mortgage",
+          count: 39
+        },
+        {
+          name: "Health Insurance",
+          count: 63
+        }
+      ]
+    },
+    {
+      name:
+        "Has a lack of transportation kept you from medical appointments, meetings, work, or from getting things needed for daily living?",
+      children: [
+        {
+          name: "yes, medical",
+          count: 197
+        },
+        {
+          name: "yes-non medical",
+          count: 59
+        },
+        {
+          name: "no",
+          count: 1091
+        }
+      ]
+    },
+    {
+      name:
+        "How often do you see or talk to people that you care about and feel close to?",
+      children: [
+        {
+          name: "I choose not to answer",
+          count: 18
+        },
+        {
+          name: "less than once a week",
+          count: 105
+        },
+        {
+          name: "1-2 times a week",
+          count: 236
+        },
+        {
+          name: "3-5 times a week",
+          count: 223
+        },
+        {
+          name: "more than 5 times a week",
+          count: 802
+        }
+      ]
+    },
+    {
+      name:
+        "Stress is when someone feels tense, nervous, anxious, or can't sleep at night because their mind is troubled. How stressed are you?",
+      children: [
+        {
+          name: "not at all",
+          count: 519
+        },
+        {
+          name: "a little bit",
+          count: 320
+        },
+        {
+          name: "somewhat",
+          count: 199
+        },
+        {
+          name: "quite a bit",
+          count: 135
+        },
+        {
+          name: "very much",
+          count: 191
+        },
+        {
+          name: "I choose not to answer",
+          count: 0
+        }
+      ]
+    },
+    {
+      name:
+        "In the past year, have you spent more than 2 nights in jail, prison, detention center, or a juvenile correctional facility?",
+      children: [
+        {
+          name: "I choose not to answer",
+          count: 11
+        },
+        {
+          name: "no",
+          count: 1350
+        },
+        {
+          name: "yes",
+          count: 21
+        }
+      ]
+    },
+    {
+      name: "Are you a refugee?",
+      children: [
+        {
+          name: "no",
+          count: 1361
+        },
+        {
+          name: "yes",
+          count: 13
+        },
+        {
+          name: "I choose not to answer",
+          count: 0
+        }
+      ]
+    },
+    {
+      name:
+        "Do you feel physically and emotionally safe where you currently live?",
+      children: [
+        {
+          name: "yes",
+          count: 1239
+        },
+        {
+          name: "no",
+          count: 76
+        },
+        {
+          name: "unsure",
+          count: 53
+        },
+        {
+          name: "I choose not to answer",
+          count: 14
+        }
+      ]
+    },
+    {
+      name:
+        "In the past year, have you been afraid of your partner or ex-partner?",
+      children: [
+        {
+          name: "yes",
+          count: 36
+        },
+        {
+          name: "no",
+          count: 1033
+        },
+        {
+          name: "unsure",
+          count: 11
+        },
+        {
+          name: "no partner in the past year",
+          count: 288
+        },
+        {
+          name: "I choose not to answer",
+          count: 0
+        }
+      ]
+    }
+  ]
+};
---
+++ praprare-data.js
@@ -0,0 +1,237 @@
+let prapareData = {
+  name: "Prapare Resources Provided",
+  children: [
+    {
+      name: "Food",
+      children: [
+        {
+          name: "Other",
+          count: 14
+        },
+        {
+          name: "Women, Infants, Children (WIC)",
+          count: 1
+        },
+        {
+          name: "More in My Basket",
+          count: 6
+        },
+        {
+          name: "Department of Social Services",
+          count: 5
+        },
+        {
+          name: "Food Pantries/Charity Kitchen",
+          count: 90
+        },
+        {
+          name: "Bull City Bucks",
+          count: 155
+        }
+      ]
+    },
+    {
+      name: "Financial",
+      children: [
+        {
+          name: "Other",
+          count: 44
+        },
+        {
+          name: "VA Benefits for Veterans",
+          count: 1
+        },
+        {
+          name: "City/County Financial Assistance",
+          count: 22
+        },
+        {
+          name: "Clothing Assistance",
+          count: 20
+        },
+        {
+          name: "Eviction/Utility Shut-Off Prevention",
+          count: 26
+        },
+        {
+          name: "Charity/Church Programs",
+          count: 22
+        },
+        {
+          name: "Legal Aid",
+          count: 9
+        }
+      ]
+    },
+    {
+      name: "Employment",
+      children: [
+        {
+          name: "Other",
+          count: 7
+        },
+        {
+          name: "Durham Literacy Center",
+          count: 1
+        },
+        {
+          name: "Vocational Rehab Center",
+          count: 8
+        },
+        {
+          name: "Dress for Success",
+          count: 10
+        },
+        {
+          name: "NC Works State Program",
+          count: 7
+        }
+      ]
+    },
+    {
+      name: "Transportation",
+      children: [
+        {
+          name: "Other",
+          count: 18
+        },
+        {
+          name: "Apply for Discounted Bus Pass",
+          count: 6
+        },
+        {
+          name: "Medicaid Transportation",
+          count: 6
+        },
+        {
+          name: "LCHC Transportation Services",
+          count: 35
+        }
+      ]
+    },
+    {
+      name: "Safety",
+      children: [
+        {
+          name: "Other",
+          count: 1
+        },
+        {
+          name: "Development of Safety Plan",
+          count: 1
+        },
+        {
+          name: "Durham Crisis Response Center",
+          count: 2
+        }
+      ]
+    },
+    {
+      name: "Housing",
+      children: [
+        {
+          name: "Other",
+          count: 10
+        },
+        {
+          name: "Durham Department of Social Services",
+          count: 5
+        },
+        {
+          name: "DHIC",
+          count: 2
+        },
+        {
+          name: "Durham County Housing Resources",
+          count: 23
+        },
+        {
+          name: "Legal Aid",
+          count: 3
+        }
+      ]
+    },
+    {
+      name: "Insurance",
+      children: [
+        {
+          name: "Medication Information",
+          count: 13
+        },
+        {
+          name: "NC MEDASSIST",
+          count: 107
+        },
+        {
+          name: "Healthcare Marketplace",
+          count: 46
+        },
+        {
+          name: "LCHC Finance Department",
+          count: 58
+        },
+        {
+          name: "Medication Assistance for Uninsured",
+          count: 12
+        },
+        {
+          name: "Department of Social Services",
+          count: 6
+        },
+        {
+          name: "Duke/UNC Charity Care",
+          count: 24
+        },
+        {
+          name: "Senior Resources",
+          count: 2
+        },
+        {
+          name: "VA Benefits for Veterans",
+          count: 2
+        },
+        {
+          name: "Senior PHARMASSIST",
+          count: 14
+        },
+        {
+          name: "Medicare Gap Coverage",
+          count: 1
+        },
+        {
+          name: "Other",
+          count: 17
+        }
+      ]
+    },
+    {
+      name: "Social/Emotional",
+      children: [
+        {
+          name: "Senior Life",
+          count: 4
+        },
+        {
+          name: "Carolina Outreach",
+          count: 4
+        },
+        {
+          name: "Alliance Behavioral Health",
+          count: 3
+        },
+        {
+          name: "Behavioral Health/Counseling Services",
+          count: 29
+        },
+        {
+          name: "El Futuro",
+          count: 11
+        },
+        {
+          name: "Other",
+          count: 3
+        }
+      ]
+    }
+  ]
+};
